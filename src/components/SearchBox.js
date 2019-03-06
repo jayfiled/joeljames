f221fb68cbaa289bc039c008e98e22892e238fe0
jayfiled <jayfiled@gmail.com>
2019-03-06 22:58:19 +1100

    a11y work inputs

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,16 +3,17 @@ import '../css/SearchBox.css';
 
 const SearchBox = ({ searchChange, killLinkOnSearch }) => {
     return (
-        <div className="search-wrap">
+        <label for="search-bar" className="search-wrap">
         {/* Make new branch and add an explicit label here */}
             <input 
+                id="search-bar"
                 type="search" 
                 placeholder="Start typing a language, eg. &quot;gatsbyJS&quot;"
                 onChange={searchChange} 
                 onMouseOver={killLinkOnSearch} 
             />
             
-        </div>
+        </label>
     );
 };
 
